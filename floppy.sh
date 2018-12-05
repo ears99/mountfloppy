b8b60a03bbd35721b90cac02d62baa928ab7ec19
Jacob S <bloggingpenguin@gmail.com>
2018-12-05 13:11:03 -0500

    Update floppy.sh

--- floppy.sh
+++ floppy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 # A bash script to mount/unmount a USB floppy drive.
+# Must be run as root
 
-if [ -a /mnt/floppy ];
-then 
-	echo Would you like to Mount or Unmount the drive?
-	
+OPTIONS="Mount Unmount"
+
+select option in $OPTIONS; do
+	if [ $REPLY = 1 ]; 
+	then 
+		echo "Mounting Drive, Please wait."
+		mount -t msdos /mnt/floppy
+		echo "Drive mounted. Please remember to unmount before removing a floppy disk."
+		exit 0
+	elif [ $REPLY = 2 ];
+	then
+		echo "Unmounting drive, Please wait."
+		umount /mnt/floppy
+		echo "Drive unmounted. You may now remove the floppy disk."
+		exit 0
+	fi
+done
